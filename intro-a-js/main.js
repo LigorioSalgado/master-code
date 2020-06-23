0597fb7847eec71bd95955b9ae7fff030f203228
Dagorik <dagorik@gmail.com>
2020-06-22 21:22:54 -0500

    variables

---
+++ intro-a-js/main.js
@@ -0,0 +1,38 @@
+// Para comentar mis lineas de código se escribe //
+// alert('Hola desde mi archivo js')
+
+// La declaración de una variable
+var x = 132
+var miVariable = 122 // number
+
+// Los tipos NUMBER tiene todas las operadores 
+var suma = x + miVariable
+var resta = x - miVariable
+var multi = x * miVariable
+var div = x / miVariable
+
+// STRINGS
+var miTexto = 'Tu compu tiene el virus: '
+var miLetra = "J"
+var miNumero = '304'
+
+//Boolean
+var verdadero = true
+var falso = false
+
+var edad = 19
+var miPassword = 'superpass1' // Ingreso el usuario
+var passwordDesdeBd = 'superpass1' // Desde la BD
+
+// Comparaciones
+// alert(edad === '19') // El triple = valida el tipo de dato y el caracter
+
+// console.log(edad === '19')
+// console.log('Mas cosas')
+// console.log('Otras cosas')
+
+var miOtraVariable = undefined
+var miOtraVariableNul = null
+
+console.log(miOtraVariable === miOtraVariableNul)
+
