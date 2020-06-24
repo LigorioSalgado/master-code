2da6e53ad48525a79dc89b6c5523dd67a74a67f3
Dagorik <dagorik@gmail.com>
2020-06-23 22:15:12 -0500

    ciclos

---
+++ ciclos/main.js
@@ -0,0 +1,12 @@
+
+console.log('Iniciando');
+
+// Donde inicia mi for, cuando se va a detener, suma de i + 1
+for(var i = 1; i <= 10000; i++) {
+    if (i%2 === 0) {
+        console.log(i);
+    }
+}
+
+console.log('Terminó');
+
