41a8c507d9676c191b8a3c27b80e378a0e30dd99
Dagorik <dagorik@gmail.com>
2020-06-23 21:15:47 -0500

    if

--- operadores-logicos/main.js
+++ operadores-logicos/main.js
@@ -1,11 +1,25 @@
-// La variable "semaforoActual" es igual a lo que escribe el usuario en el prompt
-var semaforoActual = prompt("En que color esta el semaforo")
+// // La variable "semaforoActual" es igual a lo que escribe el usuario en el prompt
+// var semaforoActual = prompt("En que color esta el semaforo")
 
-// alert(semaforoActual)
-if(semaforoActual === 'VERDE') {
-    alert('Avanza')
-} else if (semaforoActual === 'AMARILLO') {
-    alert('Acelerar')
+// // alert(semaforoActual)
+// if(semaforoActual === 'VERDE') {
+//     alert('Avanza')
+// } else if (semaforoActual === 'AMARILLO') {
+//     alert('Acelerar')
+// } else {
+//     alert('Detente')
+// }
+
+var miEdad = prompt('Cuantos años tienes') // --> Sring
+
+// Cambiar el tipo de dato de string a number
+var miEdadNumber = parseInt(miEdad) // --> Castea a number
+
+console.log(typeof miEdad)
+console.log(typeof miEdadNumber)
+
+if (miEdadNumber > 17) {
+    alert('Ya puedes manejar 😎🚗')
 } else {
-    alert('Detente')
+    alert('Aun no eres mayor de edad 😢')
 }
