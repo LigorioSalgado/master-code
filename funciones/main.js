430280443c042c4440f687ed8232f189c982a5e0
Dagorik <dagorik@gmail.com>
2020-06-24 22:02:48 -0500

    funciones

---
+++ funciones/main.js
@@ -0,0 +1,23 @@
+
+
+// Declaración de mi función
+// --> function es una palabra reservada
+// --> El nombre de mi funcion ()
+function miPrimeraFuncion() {
+    console.log('Esta es mi primera funcion')
+}
+
+// Parametros - Argumentos
+function sumaDeDosDigitos(numUno, numDos) {
+    console.log(numUno + numDos);
+}
+
+// Ejecución de mi función
+// miPrimeraFuncion()
+var digitoUnoStr = prompt('Escribe el primer numero')
+var digitoDosStr = prompt('Escribe el segundo numero')
+
+var digitoUno = parseInt(digitoUnoStr)
+var digitoDos = parseInt(digitoDosStr)
+
+sumaDeDosDigitos(digitoUno, digitoDos)
