eb952ac05e808302fe771b556e77b5fd84bad1a5
Dagorik <dagorik@gmail.com>
2020-07-08 21:21:06 -0500

    login with modals

--- login/main.js
+++ login/main.js
@@ -7,8 +7,8 @@
         // Se ejecuta cuando alguien hace click.
 
         // Hay que tomar lo que escribieron en email y password
-        var email = document.getElementById('exampleInputEmail1');
-        var password = document.getElementById('exampleInputPassword1');
+        var email = document.getElementById('exampleInputEmail1')
+        var password = document.getElementById('exampleInputPassword1')
 
         // Mis cuentas correctas --> CONSTANTES
         var EMAIL_SUCCESS = 'micorreo@gmail.com'
@@ -16,12 +16,9 @@
 
         // Validar si es correcto email & password
         if (email.value === EMAIL_SUCCESS) {
-            if (password.value === PASSWORD_SUCCESS ) {
-                // alert('Pus login')
-                
+            if (password.value === PASSWORD_SUCCESS) {
                 // Mandar a llamar al modal y activarlo
-                var modal = document.getElementById('modal-login-success');
-                // console.log(modal);
+                $('#modal-login-success').modal('show')
             } else {
                 alert('Usuario existe pero la contraseña nel')
             }
