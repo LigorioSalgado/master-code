eeece099096ade60bb6515953c9e45b10cf06bc5
Dagorik <dagorik@gmail.com>
2020-08-10 21:16:33 -0500

    destructuring

---
+++ ecma6/destructuring.js
@@ -0,0 +1,33 @@
+// // De objetos
+// const dulce = 'Si es muy dulce'
+
+// const miObj = {
+//     id: 1,
+//     name: 'Manzana',
+//     emoji: '🍎',
+//     color: 'rojo',
+//     precio: 30,
+//     dulce,
+// }
+// // console.log(miObj);
+
+// // const name = miObj.name;
+// // const id = miObj.id;
+
+// const { name, id } = miObj;
+
+// console.log(name);
+// console.log(id);
+
+
+
+// De arreglos
+const array = ['papa', 'cebolla', 'pepino']
+
+// const laPapa = array[0]
+// const elPepino = array[2]
+
+const [laPapa, laCebolla, elPepino] = array
+
+console.log(laPapa);
+console.log(elPepino);
