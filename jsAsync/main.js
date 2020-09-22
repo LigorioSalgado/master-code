d35be1746ac1f6b660505f9390d8d3a6bb2fe8b5
Dagorik <dagorik@gmail.com>
2020-09-21 20:25:31 -0500

    js async

--- jsAsync/main.js
+++ jsAsync/main.js
@@ -3,12 +3,18 @@
 // console.log(3)
 
 
-console.log(1)
+// console.log(1)
+// setTimeout(() => {
+//     console.log(2);
+// }, 3000);
+// setTimeout(() => {
+//     console.log(3);
+// }, 2000)
+// console.log(4);
+
+console.log(1);
 setTimeout(() => {
     console.log(2);
-}, 3000);
-setTimeout(() => {
-    console.log(3);
-}, 2000)
-console.log(4);
-
+}, 1000)
+for(let i = 0; i<9999999999; i++);
+console.log(3);
