3d8f9f9ae2f9b3e1db6a6afae70f5508d9b79d89
Dagorik <dagorik@gmail.com>
2020-09-21 21:58:45 -0500

    ejercicio 3

--- callbacks/main.js
+++ callbacks/main.js
@@ -1,11 +1,26 @@
-function ejemploDeCb(cb) {
-    // Aqui se puede hacer el código que sea.
-    // ...
-    cb('String que regresa el CB')
+// function ejemploDeCb(cb) {
+//     // Aqui se puede hacer el código que sea.
+//     // ...
+//     cb('String que regresa el CB')
+// }
+
+
+// ejemploDeCb((parametro) => {
+//     console.log(parametro);
+//     console.log(3+3);
+// });
+
+const operacion = (num1, num2, oppCB) => {
+    oppCB(num1, num2)
 }
 
+const suma = (numero1, numero2) => {
+    console.log(numero1 + numero2);
+}
 
-ejemploDeCb((parametro) => {
-    console.log(parametro);
-    console.log(3+3);
-});
+const resta = (numero1, numero2) => {
+    console.log(numero1 - numero2);
+}
+
+operacion(5,9, suma)
+operacion(9,5, resta)
