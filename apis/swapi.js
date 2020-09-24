771e49f5ae86a191f2d9a1ebb0fc299f04521a9a
Dagorik <dagorik@gmail.com>
2020-09-23 20:21:05 -0500

    function

--- apis/swapi.js
+++ apis/swapi.js
@@ -2,16 +2,19 @@
 // Request es una biblioteca que nos ayuda a consumir endpoints
 const request = require('request');
 
-// El metodo GET tiene dos parametros, el endpoint(URL) y un callback(contiene la respuesta)
-request.get('https://swapi.dev/api/people/1', (error, response, body) => {
-    // console.log(error);
-    console.log(response.statusCode);
+function getPeopleById(id){
+    // El metodo GET tiene dos parametros, el endpoint(URL) y un callback(contiene la respuesta)
+    request.get(`https://swapi.dev/api/people/${id}`, (error, response, body) => {
+        if (response.statusCode === 200) {
+            // PETICÓN EXITOSA
+            const bodyEnFormatoJSON = JSON.parse(body);
+            console.log(bodyEnFormatoJSON);
+        } else {
+            // ERROR
+            console.log('Tuvimos un error intenta más tarde 😢');
+        }
+    });
+}
 
-    const bodyEnFormatoJSON = JSON.parse(body);
-
-    // console.log(bodyEnFormatoJSON)
-
-    console.log(bodyEnFormatoJSON.name);
-    console.log('Ya termino de pedir la info al enpoint');
-});
+// getPeopleById(14);
 
