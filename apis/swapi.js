e183f57734722ac85b0f254205d3ef50514981bb
Dagorik <dagorik@gmail.com>
2020-09-24 11:44:08 -0500

    callback hell

--- apis/swapi.js
+++ apis/swapi.js
@@ -2,13 +2,20 @@
 // Request es una biblioteca que nos ayuda a consumir endpoints
 const request = require('request');
 
-function getPeopleById(id){
+function getPeopleById(id) {
     // El metodo GET tiene dos parametros, el endpoint(URL) y un callback(contiene la respuesta)
     request.get(`https://swapi.dev/api/people/${id}`, (error, response, body) => {
         if (response.statusCode === 200) {
             // PETICÓN EXITOSA
             const bodyEnFormatoJSON = JSON.parse(body);
-            console.log(bodyEnFormatoJSON);
+            console.log(bodyEnFormatoJSON.name);
+            console.log(bodyEnFormatoJSON.films[0]);
+
+            request.get(bodyEnFormatoJSON.films[0], (errorDos, responseDos, bodyDos) => {
+               const bodyMovieEnFormatoJSon = JSON.parse(bodyDos) 
+               console.log(bodyMovieEnFormatoJSon.title);
+            });
+
         } else {
             // ERROR
             console.log('Tuvimos un error intenta más tarde 😢');
@@ -16,5 +23,5 @@ function getPeopleById(id){
     });
 }
 
-// getPeopleById(14);
+getPeopleById(1);
 
