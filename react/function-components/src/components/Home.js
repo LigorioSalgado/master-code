309942f33d198ff21bf8f17e789038226e442798
Dagorik <dagorik@gmail.com>
2020-10-27 21:01:26 -0600

    useEffect

--- react/function-components/src/components/Home.js
+++ react/function-components/src/components/Home.js
@@ -1,13 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Counter from './Counter';
 import ListaContactos from './ListaContactos';
 import FormularioContacto from './FormularioContacto';
 function Home({saludo}) {
     
-    const [contactos, setContactos] = useState([
-        {id: 1, name: 'Mario', lastName: 'Bros'},
-        {id: 2, name: 'Luigi', lastName: 'Bros'},
-    ]);
+    const [contactos, setContactos] = useState([]);
 
     const addContacto = (nombre, lastName) => {
         console.log('El contacto que vamos a agregar es: ' + nombre);
@@ -22,13 +19,26 @@ function Home({saludo}) {
         setContactos([...contactos, objContacto]);
     }
 
+    // Funciona como didMount
+    // Pide dos parametros 1-callback, 2-array
+    useEffect(() => {
+        // Adentro del callback es didMount
+        console.log('Se ejecuto useEffect');
+        
+        // Simula una petición a aun API que tarda 3 seg en contestar
+        setTimeout(() => {
+            setContactos([{id: 1, name: 'Erwin'}, {id: 2, name: 'Lurwin'}, {id: 2, name: 'Edwino'}])
+        }, 7000)
+
+    }, []);
+
     return (
         <div>
             <h1>Este es el Home en funcion</h1>
             <p>{saludo}</p>
             {/* <Counter /> */}
             <FormularioContacto agregarContacto={addContacto} />
-            <ListaContactos lista={contactos} />
+            {contactos.length === 0 ? <h1>Cargando contactos...</h1> : <ListaContactos lista={contactos} />}
         </div>
     );
 }
