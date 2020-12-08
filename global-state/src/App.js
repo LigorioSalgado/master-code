1002b8b5b730f829175c5ea346a0c9ccae4a4bf1
Dagorik <dagorik@gmail.com>
2020-12-07 19:47:30 -0600

    global state

---
+++ global-state/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="izquierdo">
+        <h2>Lado izquierdo</h2>
+      </div>
+
+      <div className="derecho">
+        <h2>Lado derecho</h2>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
